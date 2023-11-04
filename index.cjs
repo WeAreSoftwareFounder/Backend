0bb11b5cfdb3386f850dcc9926fac568443bbb54
WeAreSoftwareFounder <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2023-11-04 19:36:53 +0000

    Corrected database string

--- index.cjs
+++ index.cjs
@@ -49,7 +49,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose
-  .connect("mongodb+srv://kirkwilkinson:Mp7pc5cwrgprmSPt@myflix.yxlavss.mongodb.net/Myflixs", {
+  .connect("mongodb+srv://kirkwilkinson:Mp7pc5cwrgprmSPt@myflix.yxlavss.mongodb.net/Myflix", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
